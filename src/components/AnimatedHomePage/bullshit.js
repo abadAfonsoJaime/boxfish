14be36e849cbea3236fbbc9d0ae8024e9692b89e
Jaime <jaime.abad.afonso@gmail.com>
2019-11-17 13:07:19 +0100

    getting started with text gradient & spring animations

--- src/components/AnimatedHomePage/bullshit.js
+++ src/components/AnimatedHomePage/bullshit.js
@@ -1,7 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { Spring } from "react-spring/renderprops";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  gradientLetters: {
+    //background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+
+    // set the background color
+    background: "linear-gradient(to right, #ff8a00 0%, #da1b60 100%)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent"
+  }
+});
 
 function Bullshit() {
+  const classes = useStyles();
   return (
     <Spring
       from={{ opacity: 0 }}
@@ -11,7 +24,7 @@ function Bullshit() {
       {props => (
         <div style={props}>
           <div style={{ fontSize: "10rem" }}>
-            <p>bullshit</p>
+            <p className={classes.gradientLetters}>bullshit</p>
           </div>
         </div>
       )}
