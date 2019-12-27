162bba3bb50ce304adf9a198c9211245dcd0ea55
abadAfonsoJaime <jaime.abad.afonso@gmail.com>
2019-12-27 01:46:30 +0100

    Updates

---
+++ precache-manifest.bdf083ffcf9ca22159b53aab5ccac50a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ab1ddb98a781c132f110350abf90d7",
+    "url": "/boxfish/index.html"
+  },
+  {
+    "revision": "9f1b64af7f6e9e8dabcc",
+    "url": "/boxfish/static/css/main.43e57e70.chunk.css"
+  },
+  {
+    "revision": "73ee1b8fe3ef3da60f58",
+    "url": "/boxfish/static/js/2.b1278bc6.chunk.js"
+  },
+  {
+    "revision": "9f1b64af7f6e9e8dabcc",
+    "url": "/boxfish/static/js/main.8cba0b83.chunk.js"
+  },
+  {
+    "revision": "25fffbc3806f5fe95489",
+    "url": "/boxfish/static/js/runtime-main.779b264f.js"
+  },
+  {
+    "revision": "8a00a0baa788717b7fe57ebd41689adc",
+    "url": "/boxfish/static/media/3dforscience_logo_black150.8a00a0ba.png"
+  },
+  {
+    "revision": "71ccc95dca5447b47a9c82f6f058161e",
+    "url": "/boxfish/static/media/founders.71ccc95d.png"
+  },
+  {
+    "revision": "709977b18836f47a99b53221b81497aa",
+    "url": "/boxfish/static/media/hardware.709977b1.png"
+  },
+  {
+    "revision": "305538202164e4dd3b3a2a91849c5237",
+    "url": "/boxfish/static/media/logo.30553820.png"
+  },
+  {
+    "revision": "56da28b59e37ea3cda8ae90efb281860",
+    "url": "/boxfish/static/media/software.56da28b5.png"
+  },
+  {
+    "revision": "5e258fcae44119d60d1cba6cb53e9c4d",
+    "url": "/boxfish/static/media/web.5e258fca.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bb353d4869f679b69738ace13b9fa08.js"
+  "/boxfish/precache-manifest.bdf083ffcf9ca22159b53aab5ccac50a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boxfish/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
